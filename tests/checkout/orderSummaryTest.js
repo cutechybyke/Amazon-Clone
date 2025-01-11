b375e8a9796fa0b51f72cf2a97f4bddec8fa09fe
Cutechybyke <cutechybyke01@gmail.com>
2025-01-11 13:18:11 -0800

    use of beforeAll hook and done functions

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,9 +1,16 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
 import {loadFromStorage, cart} from '../../data/cart.js';
+import {loadProducts} from '../../data/products.js';
 
 describe('test suite: renderOrderSummary', () => {
   const productId1 =  'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   const productId2 =  '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+  beforeAll((done) => {
+    loadProducts(() => {
+      done();
+    });
+  });
 
   beforeEach(() => {
     spyOn(localStorage, 'setItem');
@@ -11,6 +18,7 @@ describe('test suite: renderOrderSummary', () => {
       <div class="js-order-summary"></div>
       <div class="js-payment-summary"></div>
     `;
+
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([{
         productId: productId1,
@@ -23,7 +31,6 @@ describe('test suite: renderOrderSummary', () => {
       }]);
     });
     loadFromStorage();
-
 
     renderOrderSummary();
   });
